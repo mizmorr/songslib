fac686593df660b55d534f688880ec07e5a999c4
mizmorr <mizmorrr@gmail.com>
2024-11-27 02:34:24 +0300

    upd: change group field name to band to avoid fails with sql

--- internal/model/song.go
+++ internal/model/song.go
@@ -4,31 +4,31 @@ import "gorm.io/gorm"
 
 type Song struct {
 	gorm.Model
-	Group  string `gorm:"group"`
+	Band   string `gorm:"band"`
 	Name   string `gorm:"name"`
 	Lyrics string `gorm:"lyrics"`
 }
 
 type SongRequestCreate struct {
-	Group string `json:"group" binding:"required"`
-	Name  string `json:"name" binding:"required"`
+	Band string `json:"band" binding:"required"`
+	Name string `json:"name" binding:"required"`
 }
 
 type SongRequestUpdate struct {
-	Group  string `json:"group"`
+	Band   string `json:"band"`
 	Name   string `json:"name"`
 	Lyrics string `json:"lyrics"`
 }
 
 type SongResponse struct {
-	Group  string `json:"group" binding:"required"`
+	Band   string `json:"band" binding:"required"`
 	Name   string `json:"name" binding:"required"`
 	Lyrics string `json:"lyrics"`
 }
 
 func (s *Song) ToResponse() *SongResponse {
 	return &SongResponse{
-		Group:  s.Group,
+		Band:   s.Band,
 		Name:   s.Name,
 		Lyrics: s.Lyrics,
 	}
