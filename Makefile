3a5dae6a463156c7aac5bed383b4e32bd365f221
mizmorr <mizmorrr@gmail.com>
2024-11-29 02:41:20 +0300

    upd: create point in

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 run:
 		cd cmd/bin; go run main.go
+
+test:
+	go test -v -cover ./...
--- cmd/bin/main.go
+++ cmd/bin/main.go
@@ -1,4 +1,9 @@
 package main
 
+import "github.com/mizmorr/songslib/internal/app"
+
 func main() {
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
 }
